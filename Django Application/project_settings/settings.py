b553f7119ebc94b71e1d0473452ce04719aad55e
lukiod <mohakgupta0981@gmail.com>
2024-09-08 15:58:37 +0530

    test 1

--- Django Application/project_settings/settings.py
+++ Django Application/project_settings/settings.py
@@ -3,11 +3,7 @@ Django settings for project_settings project.
 """
 
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Build paths inside the project like this: os.path.join(PROJECT_DIR, ...)
 PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
